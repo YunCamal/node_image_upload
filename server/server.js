bd0cdd14c05e9d4649cea4033c28fa931ffc5e4d
Yuncamel <d1003963@gmail.com>
2022-02-20 18:19:58 +0900

    image upload commit

--- server/server.js
+++ server/server.js
@@ -1,11 +1,13 @@
+require("dotenv").config();
 const express = require('express');
 const multer = require('multer');
-
 const {v4: uuid} = require("uuid");
 const mime = require("mime-types");
-
+const mongoose = require("mongoose");
+const Image = require("./models/image");
 const app = express();
 const PORT = 5000;
+
 const storage = multer.diskStorage({
     destination: (req, file, cb) => cb(null, "./uploads"),
     filename: (req, file, cb) => 
@@ -23,7 +25,34 @@ const upload = multer({
     },
 });
 
-app.use("/uploads", express.static("uploads")); // 클라이언트에서 접속이 된다.
-app.post("/upload", upload.single("image"), (req,res) => { res.json(req.file);});
-app.listen(PORT,() => console.log("Express server listening on PORT " + PORT));
+mongoose.connect(
+    process.env.MONGO_URI,
+    {
+        useNewUrlParser: true, 
+        useUnifiedTopology: true,
+    }
+)
+.then(() => {
+    console.log("MongoDB Connected.")
+    
+    app.use("/uploads", express.static("uploads")); // 클라이언트에서 접속이 된다.
+
+    app.post("/images", upload.single("image"), async (req,res) => {
+        const image = await new Image({
+            key: req.file.filename, 
+            originalFileName: req.file.originalname 
+        }).save();
+
+        res.json(image);
+    });
+
+    app.get("/images", async(req, res) => {
+       const images = await Image.find();
+       res.json(images);
+    });
+
+    app.listen(PORT,() => console.log("Express server listening on PORT " + PORT));
+
+})
+.catch((err) => console.log(err));
 
