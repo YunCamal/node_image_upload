16d8c9f34adc3144b7ad3a8cc8692c16e01e6189
Yuncamel <d1003963@gmail.com>
2022-02-23 01:28:41 +0900

    commit

--- client/src/App.js
+++ client/src/App.js
@@ -2,12 +2,11 @@ import React from "react";
 import UploadForm from "./components/UploadForm";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-import "react-toastify/dist/ReactToastify.css";
 import ImageList from "./components/ImageList";
 
 const App = () => {
   return (
-    <div style={{maxWidth: 600, margin: "auto"}}>
+    <div style={{ maxWidth: 600, margin: "auto" }}>
       <ToastContainer/>
       <h2>사진첩</h2>
       <UploadForm />
--- client/src/components/UploadForm.js
+++ client/src/components/UploadForm.js
@@ -1,10 +1,12 @@
-import React, { useState } from "react";
+import React, { useState, useContext } from "react";
 import axios from "axios";
 import { toast } from "react-toastify";
 import "./UploadForm.css"
 import ProgressBar from "./ProjressBar";
+import { ImageContext } from "../context/ImageContext";
 
 const UploadForm = () => {
+    const [images, setImages] = useContext(ImageContext);
     const defaultFileName = "이미지 파일을 업로드 해주세요.";
     const [file, setFile] = useState(null);
     const [imgSrc, setImgSrc] = useState(null);
@@ -32,6 +34,7 @@ const UploadForm = () => {
                 }
             });
             
+            setImages([...images, res.data ]);
             toast.success("이미지 업로드 성공!");
             setTimeout(() =>{
                 setPercent(0);
