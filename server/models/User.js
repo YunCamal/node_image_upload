620296f55b820de9b907e26e4b5347fa0b3f73d6
Yuncamel <d1003963@gmail.com>
2022-03-01 23:39:20 +0900

    commit

--- server/models/User.js
+++ server/models/User.js
@@ -4,7 +4,12 @@ const UserSchema = new mongoose.Schema(
     {
         name :{ type: String, required: true },
         username :{ type: String, required: true,unique: true },
-        password :{ type: String, required: true, },
+        hashedPassword :{ type: String, required: true, },
+        sessions: [
+            {
+                createAt: { type: Date, required: true },
+            },
+        ],
     },
     { timestamps: true }
 );
--- server/routes/userRouter.js
+++ server/routes/userRouter.js
@@ -1,10 +1,49 @@
 const { Router } = require("express");
 const userRouter = Router();
 const User = require("../models/User");
+const { hash, compare } = require("bcryptjs");
+
 
 userRouter.post("/register", async(req, res) => {
-    await new User(req.body).save();
-    res.json({ message: "user registerd" });
+    try {
+        if(req.body.password.length < 6) throw new Error("비밀번호를 6자 이상으로 해주세요.");
+        if(req.body.username.length < 3) throw new Error("username을 3자 이상으로 해주세요.");
+
+        const hashedPassword = await hash(req.body.password, 10);
+    
+        const user = await new User({
+            name: req.body.name,
+            username: req.body.username,
+            hashedPassword,
+            sessions: [{ createAt: new Date()}]
+        }).save();
+        const session = user.sessions[0];
+        res.json({ message: "user registerd", seesionId: session._id, name: user.name, });
+    
+        } catch(err) {
+            res.status(400).json({ message: err.message })    
+        }
+});
+
+userRouter.patch("/login",async(req, res) => {
+    try {
+        const user = await User.findOne({ username: req.body.username });
+        const isValid = await compare(req.body.password, user.hashedPassword);
+        if (!isValid) throw new Error("입력하신 정보가 올바르지 않습니다.");
+        user.sessions.push({ createdAt: new Date() });
+        const session = user.sessions[user.seesions.length-1];
+        await user.save();
+
+        res.json({ 
+            message: "user validated!", 
+            sessionId: session._id, 
+            name: user.name, 
+        });
+
+    } catch (error) {
+        res.status(400).json({ message: error.message, });
+    }
+
 });
 
 module.exports = { userRouter };
